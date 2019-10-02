fda5d6c0e643e53123ddd284a70b7f3e6196a746
Bryan Hoyt <bryan.hoyt@daugherty.com>
2019-10-02 09:37:34 -0500

    Added beerRouter and monggose

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const beerRouter = require('./routes/beerRouter')
+const mongoose = require('mongoose')
+
+app.get('/beers', beerRouter)
 
 app.use('/', (req, res) => {
     console.log(`Something hit me!`)
