f1bfc4beceba18305722c6b2a6f68ca3cc8d4a97
Bryan Hoyt <bryan.hoyt@daugherty.com>
2019-10-02 10:36:43 -0500

    This commit is too big

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require('express')
 const app = express()
 const beerRouter = require('./routes/beerRouter')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-app.get('/beers', beerRouter)
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/beers', beerRouter)
 
 app.use('/', (req, res) => {
     console.log(`Something hit me!`)
     res.send(`<h1>Welcome to the beerAPI</h1>`)
+})
+
+mongoose.connect('mongodb://localhost:27017/beers', {
+    userNewUrlParser: true
+})
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to the beers database!`)
+})
+mongoose.connection.on('error', (err) => {
+    console.log(`Error connection to beers database:\n${err}`)
 })
 
 const port = process.env.PORT || 4444
